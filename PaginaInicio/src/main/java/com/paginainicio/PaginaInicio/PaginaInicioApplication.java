a8c8580c64c34eb971e590f5ce1130a02d718415
Alan <alan.fernandez@ucb.edu.bo>
2025-04-06 22:51:53 -0400

    Empezando con la pagina de inicio en Spring boot

---
+++ PaginaInicio/src/main/java/com/paginainicio/PaginaInicio/PaginaInicioApplication.java
@@ -0,0 +1,13 @@
+package com.paginainicio.PaginaInicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaInicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaInicioApplication.class, args);
+	}
+
+}
